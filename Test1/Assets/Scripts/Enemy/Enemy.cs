e665c55794912b94f72743a7219048f680045756
ps20alucas <44873386+ps20alucas@users.noreply.github.com>
2019-09-30 14:06:32 -0400

    gfaswgzaeswqw
    bhjnFA

---
+++ Test1/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int enemyHealth;
+    public int enemySpeed;
+    public bool enemyFlight;
+    public bool enemyInvincible;
+    public int enemyJumpHeight;
+
+    void Update()
+    {
+
+    }
+    public void EnemyAttack1()
+    {
+
+    }
+}

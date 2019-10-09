240eea73407c628e7c0e58528f7fc88cad9ba518
GreatHips <49949549+GreatHips@users.noreply.github.com>
2019-10-09 12:50:31 -0400

    d

--- Test1 - Jared/Assets/Scripts/Objects/HeartCollect.cs
+++ Test1 - Jared/Assets/Scripts/Objects/HeartCollect.cs
@@ -7,17 +7,33 @@ public class HeartCollect : MonoBehaviour
     public GameObject player;
     HealthManager healthManager;
 
+    public bool notMaxHealth = false;
 
+    public void Update()
+    {
+        if (player.GetComponent<HealthManager>().health < player.GetComponent<HealthManager>().healthMax)
+        {
+            notMaxHealth = true;
+        }
+        if (player.GetComponent<HealthManager>().health == player.GetComponent<HealthManager>().healthMax)
+        {
+            notMaxHealth = false;
+        }
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject == player && player.GetComponent<HealthManager>().health < player.GetComponent<HealthManager>().healthMax)
+        if (collision.gameObject == player)
         {
-            
-            player.GetComponent<HealthManager>().Heal(20);
-            Destroy(gameObject);
-        } else
+            {
+                player.GetComponent<HealthManager>().Heal(20);
+                Destroy(gameObject);
+            }
+
+        }
+        if (!notMaxHealth)
         {
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
+
     }
 }

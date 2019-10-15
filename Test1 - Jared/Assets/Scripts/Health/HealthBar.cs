f9502932b57ec87fd673c87ad58ebf714ff1e7a1
GreatHips <49949549+GreatHips@users.noreply.github.com>
2019-10-15 12:40:36 -0400

    o

--- Test1 - Jared/Assets/Scripts/Health/HealthBar.cs
+++ Test1 - Jared/Assets/Scripts/Health/HealthBar.cs
@@ -10,7 +10,7 @@ public class HealthBar : HealthManager
     public GameObject healthBarsBackground;
     public Vector3 healthBarsBackgroundScale;
 
-    public float timeSinceAttacked = -6;
+   
     void Start()
     {
         // sets healthBar vector3 to the scale of the healthbar
@@ -23,12 +23,10 @@ public class HealthBar : HealthManager
 
     public void Update()
     {
-        
-        //timeSinceAttacked -= Time.deltaTime;
 
         // if the tag is an enemy
         if (gameObject.tag == "Enemy")
-        { 
+        {
             healthBar.y = 20f;
             healthBarsBackgroundScale.y = 10f;
             healthBar.x = health * 3f;
@@ -53,37 +51,12 @@ public class HealthBar : HealthManager
             healthBarsBackgroundScale.y = .25f;
             healthBar.x = health / 15;
             healthBarsBackgroundScale.x = healthMax / 15;
-           
+
             healthBars.transform.localScale = healthBar;
-            
+
             healthBarsBackground.transform.localScale = healthBarsBackgroundScale;
         }
-       // timeComponents();
-    }
-    /*
-    void timeComponents()
-    {
-        if (timeSinceAttacked <= -5 && gameObject.tag == "Enemy" || gameObject.tag == "Boss")
-        {
-            overallHealthBar.SetActive(false);
-            healthBars.SetActive(false);
-            healthBarsBackground.SetActive(false);
-        }
-        else if (timeSinceAttacked > -10 && gameObject.tag == "Enemy" || gameObject.tag == "Boss")
-        {
-            overallHealthBar.SetActive(true);
-            healthBars.SetActive(true);
-            healthBarsBackground.SetActive(true);
-        }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Bullets" && gameObject.tag == "Boss" || gameObject.tag == "Enemy")
-        {
-            timeSinceAttacked = 0;
-        }
+        timer.timeComponents();
     }
-    */
-
 }

6694f8f54d1d08b56192e84753ba1116dcff63ca
GreatHips <49949549+GreatHips@users.noreply.github.com>
2019-10-23 12:28:33 -0400

    d

--- Test1 - Jared/Assets/Scripts/Enemy/Controller2D.cs
+++ Test1 - Jared/Assets/Scripts/Enemy/Controller2D.cs
@@ -98,9 +98,10 @@ public class Controller2D : RaycastController
                     ClimbSlope(ref moveAmount, slopeAngle, hit.normal);
                     moveAmount.x += distanceToSlopeStart * directionX;
                 }
-
+                // fix this
                 if (!collisions.climbingSlope || slopeAngle > maxSlopeAngle)
                 {
+                    
                     moveAmount.x = (hit.distance - skinWidth) * directionX;
                     rayLength = hit.distance;
 
@@ -136,11 +137,14 @@ public class Controller2D : RaycastController
                 {
                     if (directionY == 1 || hit.distance == 0)
                     {
+
                         continue;
+                        
                     }
                     if (collisions.fallingThroughPlatform)
                     {
-                        continue;
+
+                        continue; 
                     }
                     if (playerInput.y == -1)
                     {
@@ -155,6 +159,7 @@ public class Controller2D : RaycastController
 
                 if (collisions.climbingSlope)
                 {
+
                     moveAmount.x = moveAmount.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(moveAmount.x);
                 }
 
@@ -182,7 +187,7 @@ public class Controller2D : RaycastController
             }
         }
     }
-
+    
     void ClimbSlope(ref Vector2 moveAmount, float slopeAngle, Vector2 slopeNormal)
     {
         float moveDistance = Mathf.Abs(moveAmount.x);
